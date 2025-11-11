c9c83d8b727e162bed6837e69ace8d6a211f483c
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-11 19:00:09 -0300

    Update Questao2.js

--- src/Questao2.js
+++ src/Questao2.js
@@ -1,1 +1,21 @@
+const jsonString = `{
+  "usuarios": [
+    {"nome": "Carlos", "email": "carlos@email.com"},
+    {"nome": "Maria", "email": "maria@email.com"},
+    {"nome": "João", "email": "joao@email.com"}
+  ]
+}`;
+
+const dados = JSON.parse(jsonString);
+
+let listaEmails = [];
+
+for (const usuario of dados.usuarios) {
+
+  listaEmails.push(usuario.email);
+}
+
+const emails = listaEmails.join(", ");
+
+console.log(emails);
 
