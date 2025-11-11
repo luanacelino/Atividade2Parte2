265c6f527c44584e89fec20eadef30f8814f6968
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-11 19:07:37 -0300

    Update Questao3.js

--- src/Questao3.js
+++ src/Questao3.js
@@ -1,1 +1,28 @@
+const produtosJSON = `{
+  "produtos": [
+    {"nome": "Mouse", "preco": 25.90},
+    {"nome": "Teclado", "preco": 89.90},
+    {"nome": "Monitor", "preco": 450.00},
+    {"nome": "Cabo USB", "preco": 15.00}
+  ]
+}`;
 
+function produtosAcimaDe50(jsonString) {
+
+  const dados = JSON.parse(jsonString);
+
+  let nomes = [];
+
+  for (const produto of dados.produtos) {
+    
+    if (produto.preco > 50) {
+    
+      nomes.push(produto.nome);
+    }
+  }
+
+  return nomes;
+}
+
+const resultado = produtosAcimaDe50(produtosJSON);
+console.log(resultado);
