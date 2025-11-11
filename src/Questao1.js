62accfbe375ea94c9f894b6a946d1b4479a98847
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-11 18:56:08 -0300

    Update Questao1.js

--- src/Questao1.js
+++ src/Questao1.js
@@ -1,1 +1,14 @@
+```
+const usuario = {
+  nome: "Ana Silva",
+  idade: 30,
+  habilidades: ["JavaScript", "React", "Node.js"],
+  ativo: true
+};
 
+const jsonString = JSON.stringify(usuario); // Objeto → JSON
+const usuarioRecuperado = JSON.parse(jsonString); // JSON → Objeto
+
+console.log(jsonString);
+console.log(usuarioRecuperado);
+```
