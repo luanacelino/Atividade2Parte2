4a5adc59e2cedd0a6850c264efdcf4a022cd682b
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-11 19:14:20 -0300

    Update Questao6.js

--- src/Questao6.js
+++ src/Questao6.js
@@ -1,1 +1,14 @@
+function parseJSONSafe(jsonString) {
+  try {
+    
+    const resultado = JSON.parse(jsonString);
+    return resultado; 
+  } catch (erro) {
+  
+    console.error("Erro ao converter JSON:", erro.message);
+    return null;
+  }
+}
 
+const jsonInvalido = '{nome: "João", "idade": 30}'; 
+console.log(parseJSONSafe(jsonInvalido));
