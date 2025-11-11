ad9c1372036c4c44e7a010b19453b2dcc4e88ce6
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-11 19:09:21 -0300

    Update Questao4.js

--- src/Questao4.js
+++ src/Questao4.js
@@ -1,1 +1,15 @@
+const endereco = {
+  rua: "Av. Paulista",
+  numero: 1000,
+  cidade: "São Paulo",
+  cep: "01310-100"
+};
+
+function formatarEndereco(obj) {
+
+  return `${obj.rua}, ${obj.numero} - ${obj.cidade} - CEP: ${obj.cep}`;
+}
+
+const resultado = formatarEndereco(endereco);
+console.log(resultado);
 
